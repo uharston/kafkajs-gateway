7f81aff5ac947fd4ff130df137f35a04aa12d8c4
Uriah Harston <uharston@gmail.com>
2021-04-15 11:35:31 -0500

    Add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const options = new DocumentBuilder()
+    .setTitle('Indie Tech User Account Service')
+    .setVersion('1.0.0')
+    .addTag('User Account Service', 'This s')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.DEV_PORT || 5000;
+  await app.listen(port);
+  Logger.log(`Server running on http://localhost:${port}`, 'Bootstrap');
 }
 bootstrap();
