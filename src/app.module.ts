c309f8a1caf65a3fa80299507fb8738dbc848c06
Uriah Harston <uharston@gmail.com>
2021-04-16 16:22:29 -0500

    Create Kafka client config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ApiModule } from './api/api.module';
 import { SchemaFirstModule } from './schema-first/schema-first.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { BROKER1, CLIENTID, MY_CONT_SERVICE } from './constants';
+import { CodeFirstModule } from './code-first/code-first.module';
 
 @Module({
-  imports: [ApiModule, SchemaFirstModule],
+  imports: [
+    ClientsModule.register([
+      {
+        name: MY_CONT_SERVICE,
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: CLIENTID,
+            brokers: [BROKER1],
+          },
+          producer: {},
+          // consumer: {
+          //   groupId: 'myCont-consumer',
+          // },
+        },
+      },
+    ]),
+    SchemaFirstModule,
+    CodeFirstModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
